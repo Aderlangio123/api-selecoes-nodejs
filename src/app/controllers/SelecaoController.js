100f4cc2ed400254a04734e7a38934f958b59114
Aderlangio123 <faderlangio123@gmail.com>
2026-03-31 12:57:39 -0300

    alterações nas funcções do crud

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -2,7 +2,6 @@ import SelecaoRepository from "../repositories/SelecaoRepository.js";
 
 class SelecaoController {
   async index(req, res) {
-    // res.status(200).send(selecoes)
     try {
       const row = await SelecaoRepository.findAll();
       res.json(row); 
@@ -13,33 +12,48 @@ class SelecaoController {
   }
 
   async show(req, res) {
-    // res.json(buscarSelecaoPorId(req.params.id))
-    const id = req.params.id;
-    const row = await SelecaoRepository.findById(id);
-    res.json(row);
+    try {
+      const id = req.params.id;
+      const row = await SelecaoRepository.findById(id);
+      res.json(row);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({erro: error})
+    }
   }
   async store(req, res) {
-    // selecoes.push(req.body)
-    // res.status(201).send("Seleção cadastrada!")
-    const selecao = req.body;
-    const row = await SelecaoRepository.create(selecao)
-    res.json(row)
+    try {
+      const selecao = req.body;
+      const row = await SelecaoRepository.create(selecao)
+      res.json(row)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({erro: error})
+    }
     
   }
   async update(req, res) {
-    const id = req.params.id;
-    const selecao = req.body;
-    const row = await SelecaoRepository.update(selecao, id)
-    res.json(row)
+      try {
+        const id = req.params.id;
+        const selecao = req.body;
+        const row = await SelecaoRepository.update(selecao, id)
+        res.json(row)
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({erro: error})
+      }
+
   }
   
   async delete(req, res) {
-    // let index = buscarIndexSelecao(req.params.id)
-    // selecoes.splice(index, 1)
-    // res.send(`Seleção com id ${req.params.id} excluída com sucesso!`)
-    const id = req.params.id;
-    const row = await SelecaoRepository.delete(id)
-    res.json(row)
+    try {
+        const id = req.params.id;
+        const row = await SelecaoRepository.delete(id)
+        res.json(row)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({erro: error})
+    }
 }
 }
 
