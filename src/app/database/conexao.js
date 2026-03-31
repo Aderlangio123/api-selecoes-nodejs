46fa863e9482abe49dc815c64c309dee8d06344f
Aderlangio123 <faderlangio123@gmail.com>
2026-03-30 23:14:15 -0300

    concertos para deploy

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,12 +1,12 @@
 import mysql from 'mysql2'
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user : "root",
-    password : "root",
-    database: "bd_copa"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 conexao.connect()
 
